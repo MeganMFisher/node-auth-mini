548d98e0163c7a9ea11954e8cbce56737e4b6ee1
MeganMFisher <MeganMichelleFisher@gmail.com>
2017-09-06 11:40:02 -0600

    done

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    domain: 'meganfisher43.auth0.com',
+    clientID: 'kXu57Q10vvaGr1AbWRh7XKGA1bXGpOu3',
+    clientSecret: '1OdwSLKzGm5Ml_52uAerHzCedxdqTBzK8RnfP96AB56V7AqldLGrDV1Vm_eZqh5r',
+  };
